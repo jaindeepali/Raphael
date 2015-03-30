0b5d95c9c8301ae9c7af764e7e6599fe7372ed47
jaindeepali <djdeepalijain811@gmail.com>
2015-03-30 15:56:15 +0530

    Add SIFT feature Extractor

---
+++ featureExtactor.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+def SIFT ( img ):
+	sift = cv2.SIFT()
+	kp = sift.detect( img, None )
+	print kp[0]
+
+	img = cv2.drawKeypoints( img, kp, flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS )
+
+	cv2.imwrite( 'images/sample_sift_keypoints.png', img)
+
+if __name__ == "__main__":
+
+	img = cv2.imread('images/sample.png')
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	SIFT( gray )
