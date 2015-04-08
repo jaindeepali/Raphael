12b04966d9a609de69ea3f168adcbc576f36c636
jaindeepali <djdeepalijain811@gmail.com>
2015-04-08 17:28:16 +0530

    Add sample data

--- featureExtactor.py
+++ featureExtactor.py
@@ -8,10 +8,15 @@ def SIFT ( img ):
 
 	img = cv2.drawKeypoints( img, kp, flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS )
 
-	cv2.imwrite( 'images/sample_sift_keypoints.png', img)
+	cv2.imwrite( 'data/sample/sample_sift_keypoints.png', img)
+
+def getBrightness ( img ):
+	avg = np.average( img )
+	return avg
 
 if __name__ == "__main__":
 
-	img = cv2.imread('images/sample.png')
+	img = cv2.imread('data/sample/sample.png')
 	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	print getBrightness( gray )
 	SIFT( gray )
