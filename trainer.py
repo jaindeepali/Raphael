5111c9120867cecd47e085afa473a3b4808d7d9e
jaindeepali <djdeepalijain811@gmail.com>
2015-04-10 17:12:35 +0530

    Add brightness to features

--- trainer.py
+++ trainer.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import os
+from datetime import datetime
 from scipy.cluster.vq import *
 from featureExtractor import featureExtractor
 from helper import *
@@ -18,9 +19,11 @@ class trainer():
 		# Get features of all training images
 
 		classes = os.listdir( self.training_path )
+		cid = 1
 		for c in classes:
 			class_path = os.path.join( self.training_path, c )
 			images = os.listdir( class_path )
+			cid = cid + 1
 			for i in images:
 				image = {}
 
@@ -35,6 +38,7 @@ class trainer():
 				image['no_of_descriptors'] = len(des)
 				image['class'] = c
 				image['brightness'] = f.brightness()
+				image['cid'] = cid
 
 				if self.descriptor_pool == None :
 					self.descriptor_pool = des
@@ -48,7 +52,11 @@ class trainer():
 
 		for img in self.image_data:
 			hist = createHistogram( img['descriptors'], self.voc, k )
-			img['histogram'] = hist
+			img['SIFThistogram'] = hist
+			img['features'] = hist
+			print type( img['features'] )
+			print type( img['brightness'] )
+			img['features'] = np.append( img['features'], img['brightness'] )
 
 	def clusterSIFTDescriptors( self, k ):
 
@@ -57,6 +65,8 @@ class trainer():
 
 if __name__ == '__main__' :
 	path = 'data/training'
+	print 'Script started at', datetime.now()
 	f = trainer( path )
 	f.getFeatures()
+	print 'Script finished at', datetime.now()
 	print f.image_data
