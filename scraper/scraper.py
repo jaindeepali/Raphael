bd48bce53b4295a3ae71691ca6022904ce54bbc4
Divij Bindlish <dvjbndlsh93@gmail.com>
2015-04-18 03:17:28 +0530

    Adds initial version of scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,8 +1,24 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 import requests
 
+base_url = 'http://www.wikiart.org'
+
+def get( endpoint ):
+	response = requests.get( base_url + endpoint )
+	soup = BS(response.text)
+	return soup
+
 def scrape():
-	base_url = 'http://www.wikiart.org/'
+	century_list_page = get( '/en/artists-by-century' )
+	for century_element in century_list_page.select( 'h6.artist-grouped a' ):
+		century = century_element.text[0:2]
+		century_page = get( century_element['href'] )
+
+		pager = century_page.select( 'div.pager-row.mb5' )
+		if len(pager) == 0:
+			artist_list = century_page.select()
+		if len(pager) > 0:
+			print century
 
 if __name__ == '__main__':
 	scrape()
