1b9f35f2bc987f9cddf4cf731886164d960bc1c0
jaindeepali <djdeepalijain811@gmail.com>
2015-04-18 18:32:03 +0530

    Classification using Neural Network

--- classifier.py
+++ classifier.py
@@ -14,9 +14,9 @@ from sklearn.lda import LDA
 from sklearn.qda import QDA
 from sklearn.preprocessing import StandardScaler
 
-class trainer():
+class classifier():
 
-	def __init__( self, training_path, testing_path ):
+	def __init__( self, training_path, testing_path, number_of_classes = 3 ):
 		self.training_path = training_path
 		self.testing_path = testing_path
 		self.training_image_list = []
@@ -24,8 +24,9 @@ class trainer():
 		self.training_labels = []
 		self.testing_labels = []
 		self.class_map = {}
+		self.n_classes = number_of_classes
 		self.classifiers = {
-			'knn': KNeighborsClassifier(3),
+			'knn': KNeighborsClassifier( 3 ),
 		    'svm_linear': SVC(kernel="linear", C=0.025),
 		    'svm': SVC(gamma=2, C=1),
 		    'tree': DecisionTreeClassifier(max_depth=5),
@@ -34,13 +35,12 @@ class trainer():
 		    'gauss': GaussianNB(),
 		    'lda': LDA(),
 		    'qda': QDA(),
-		    'ann': neuralNetwork() }
+		    'ann': neuralNetwork( self.n_classes ) }
 
 	def get_training_image_list( self ):
 		classes = os.listdir( self.training_path )
 		cid = 0
 		for c in classes:
-			cid = cid + 1
 			self.class_map[cid] = c
 			class_path = os.path.join( self.training_path, c )
 			images = os.listdir( class_path )
@@ -49,6 +49,7 @@ class trainer():
 			for i in images:
 				img_path = os.path.join( class_path, i )
 				self.training_image_list.append( img_path )
+			cid = cid + 1
 
 	def get_testing_image_list( self ):
 		images = os.listdir( self.testing_path )
@@ -68,23 +69,22 @@ class trainer():
 		self.preprocess( features )
 		features = self.scaler.transform( features );
 		self.classifiers[ classifier ].fit( features, self.training_labels )
-		self.testing_labels = self.classifiers[ classifier ].predict( features )
 
 	def classify( self, classifier ):
 		self.train( classifier )
-		# self.get_testing_image_list()
-		# f = featurePooling( self.testing_image_list, 1 )
-		# f.getFeatures( self.voc )
-		# features = f.features
-		# features = self.scaler.transform( features )
-		# self.testing_labels = self.classifiers[ classifier ].predict( features )
+		self.get_testing_image_list()
+		f = featurePooling( self.testing_image_list, 1 )
+		f.getFeatures( self.voc )
+		features = f.features
+		features = self.scaler.transform( features )
+		self.testing_labels = self.classifiers[ classifier ].predict( features )
 
 
 if __name__ == '__main__' :
 	training_path = 'data/training'
 	testing_path = 'data/testing'
 	print 'Script started at', datetime.now()
-	f = trainer( training_path, testing_path )
+	f = classifier( training_path, testing_path, 3 )
 	# f.classify( 'knn' )
 	# f.classify( 'svm_linear' )
 	# f.classify( 'svm' )
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,29 +1,39 @@
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.datasets import SupervisedDataSet
-from pybrain.structure import TanhLayer
+from pybrain.datasets import ClassificationDataSet
+from pybrain.structure import SoftmaxLayer
 from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.utilities import percentError
 import numpy as np
 
 class neuralNetwork():
 
-	def __init__( self ):
-		self.net = None
+	def __init__( self, n_classes ):
+		self.n_classes = n_classes
 
 	def fit( self, X, Y ):
 		n_features = X.shape[1]
-		self.ds = SupervisedDataSet( n_features, 1 )
+		self.train_ds = ClassificationDataSet( n_features, 1, nb_classes = self.n_classes )
 		for train, target in zip( X, Y ):
-			self.ds.addSample( train, target )
+			self.train_ds.addSample( train, [target] )
 
-		self.net = buildNetwork( n_features, 2*n_features, 1, hiddenclass=TanhLayer )
-		self.trainer = BackpropTrainer( self.net, self.ds )
-		self.trainer.train()
+		self.train_ds._convertToOneOfMany( )
+
+		self.net = buildNetwork( self.train_ds.indim, 2*n_features, self.train_ds.outdim, outclass = SoftmaxLayer )
+		self.trainer = BackpropTrainer( self.net, self.train_ds )
 
 	def predict( self, X ):
-		labels = []
+		n_features = X.shape[1]
+		self.test_ds = ClassificationDataSet( n_features, 1, nb_classes = self.n_classes )
 		for test in X:
-			labels.append( self.net.activate( test )[0] )
-		return labels
+			self.test_ds.addSample( test, [1] )
+
+		self.test_ds._convertToOneOfMany( )
+
+		for i in range( 100 ):
+			self.trainer.trainEpochs( 5 )
+			self.labels = self.net.activateOnDataset( self.test_ds )
+			self.labels = self.labels.argmax(axis=1)
+		return self.labels
 
 if __name__ == '__main__':
 	X = np.array([[ 0.,  0.],
@@ -32,9 +42,7 @@ if __name__ == '__main__':
 		[ 1.,  1.]])
 	Y = [ 0, 1, 1, 0 ]
 	test = np.array([[ 0.,  0.],
-		[ 0.,  1.],
-		[ 1.,  0.],
-		[ 1.,  1.]])
-	n = neuralNetwork()
+		[ 0.,  1.]])
+	n = neuralNetwork( 2 )
 	n.fit( X, Y )
 	print n.predict( test )
