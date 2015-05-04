34f90bd63a6e155bceec9065343e0fae68e07d40
Divij Bindlish <dvjbndlsh93@gmail.com>
2015-05-05 03:15:09 +0530

    Encode unicode URLs before fetching

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup as BS
 import requests
 import urllib
+import re
+import urlparse
 import os
 
 base_url = 'http://www.wikiart.org'
@@ -9,6 +11,27 @@ def get( endpoint ):
 	response = requests.get( base_url + endpoint )
 	soup = BS( response.text )
 	return soup
+
+def _unicode( string ):
+	if isinstance( string, unicode ):
+		result = string
+	else:
+		try:
+			result = unicode( string, encoding='utf-8' )
+		except UnicodeDecodeError:
+			result = string.decode( 'latin1' ).encode( 'utf-8', 'replace' )
+
+	return result
+
+def urlEncodeNonAscii(b):
+    return re.sub('[\x80-\xFF]', lambda c: '%%%02x' % ord(c.group(0)), b)
+
+def iriToUri(iri):
+    parts= urlparse.urlparse(iri)
+    return urlparse.urlunparse(
+        part.encode('idna') if parti==1 else urlEncodeNonAscii(part.encode('utf-8'))
+        for parti, part in enumerate(parts)
+    )
 
 def scrape():
 	art_movement_list_page = get( '/en/artists-by-art-movement' )
@@ -25,7 +48,7 @@ def scrape():
 				path = os.path.join( '..', 'data', 'training', art_movement )
 				if not os.path.exists( path ): os.mkdir( path )
 				path = os.path.join( path , str( i ) + '.jpg' )
-				urllib.urlretrieve( painting_url, path )
+				urllib.urlretrieve( iriToUri( _unicode( painting_url ) ), path )
 				print painting_url + ' retrieved'
 				i = i + 1
 
