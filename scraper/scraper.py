88115816725c64295e1e64ec6177a3c8b5f6e5c0
jaindeepali <djdeepalijain811@gmail.com>
2015-05-05 00:35:39 +0530

    Scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup as BS
 import requests
+import urllib
+import os
 
 base_url = 'http://www.wikiart.org'
 
@@ -9,16 +11,23 @@ def get( endpoint ):
 	return soup
 
 def scrape():
-	century_list_page = get( '/en/artists-by-century' )
-	for century_element in century_list_page.select( 'h6.artist-grouped a' ):
-		century = century_element.text[0:2]
-		century_page = get( century_element['href'] )
+	art_movement_list_page = get( '/en/artists-by-art-movement' )
+	for art_movement_element in art_movement_list_page.select( 'h6.artist-grouped a' ):
+		art_movement = art_movement_element.text
+		art_movement_page = get( art_movement_element['href'] )
 
-		pager = century_page.select( 'div.pager-row.mb5' )
-		if len(pager) == 0:
-			artist_list = century_page.select()
-		if len(pager) > 0:
-			print century
+		i = 1
+		for artist_element in art_movement_page.select( 'p.bigtext a' ):
+			artist = artist_element.text
+			artist_page = get( artist_element['href'] )
+			for painting_element in artist_page.select( 'a.rimage img' ):
+				painting_url = painting_element['src']
+				path = os.path.join( '..', 'data', 'training', art_movement )
+				if not os.path.exists( path ): os.mkdir( path )
+				path = os.path.join( path , str( i ) + '.jpg' )
+				urllib.urlretrieve( painting_url, path )
+				print painting_url + ' retrieved'
+				i = i + 1
 
 if __name__ == '__main__':
 	scrape()
